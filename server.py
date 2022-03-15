a0b80fe177464031fbc2f1df52c6e217e6896be5
leskoarpi <71691273+leskoarpi@users.noreply.github.com>
2022-03-15 11:36:23 +0100

    Add files via upload

---
+++ server.py
@@ -0,0 +1,55 @@
+import cv2
+import socket
+import mediapipe as mp
+import time
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((socket.gethostname(),1234))
+s.listen(2)
+clientsocket, adress = s.accept()
+
+
+cap = cv2.VideoCapture(0)
+cap.set(3, 700)
+cap.set(4, 500)
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
+
+
+
+while True:
+    success, img = cap.read()
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+
+
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            for id, lm in enumerate(handLms.landmark):
+                h, w, c = img.shape
+                cx, cy = int(lm.x*w), int(lm.y*h)
+                
+                
+            
+               
+                
+                if 'Left' in str(results.multi_handedness[0]) and id == 8 and clientsocket:
+                    adat = (0,cy)
+                    clientsocket.send(bytes(str(adat),'utf-8'))
+                    
+                if 'Right' in str(results.multi_handedness) and id == 8 and clientsocket:
+                    adat = (1, cy)
+                    clientsocket.send(bytes(str(adat),'utf-8'))
+                    
+                    
+                    
+    #cv2.imshow('image', img)
+    cv2.waitKey(1)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
